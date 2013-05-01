ce87109bdc1b476a260bf59e9470ed0c8668c17c
Trace <trace_hagan@yahoo.com>
2013-04-30 21:59:51 -0700

    updated Walker class per Issue #11 suggestion

--- src/net/snakedoc/superd/Walker.java
+++ src/net/snakedoc/superd/Walker.java
@@ -27,47 +27,64 @@ import java.io.IOException;
 public class Walker {
     private static int BUFFER;
     private static final Logger log = Logger.getLogger(DedupeR.class);
+    private static Hasher hasher = new Hasher();
+    private static DedupeSQL sql = new DedupeSQL();
+    private static Config config = new Config("props/superD.properties");
 
     public Walker(int buffer){
         this.BUFFER = buffer;
     }
 
     public static void walk(File path){
-
-        Hasher hasher = new Hasher();
-        DedupeSQL sql = new DedupeSQL();
-        Config config = new Config("props/superD.properties");
-        int i=0;
-
         File[] contents = path.listFiles();
 
         for (File curFile : contents){
             try{
-                if (curFile.isDirectory() && (curFile != null) && !curFile.isHidden()){
+                if (isValidDirectory(curFile)){
                     walk(curFile);
-                } else if (!curFile.isDirectory() && !curFile.isHidden() && curFile != null ){
-
-                    //reads in HASH_ALGO from Prop File to decide which algorithm to use
-                    String hashAlgo = new String(config.getConfig("HASH_ALGO"));
-                    String file = "";
-                    String hash = "";
-                    try {
-                        file = curFile.getPath();
-                        log.debug("File: " + file);
-                        hash = hasher.getHash(curFile.getPath(), hashAlgo, BUFFER);
-                    } catch (IOException | HasherException e1) {
-                        log.error("Failed to access and/or hash file!", e1);
-                    }
-
-                    sql.writeRecord(file, hash);
-
-                    log.debug("\n\t Hash: " + hash);
+                } else if (isValidFile(curFile)){
+                    hashFile(curFile);
                 }
             } catch (Exception e) {
                 log.warn("Failed to access file!", e);
-                log.debug("i: " + i + "  |  path: " + contents[i].getPath() +
+                log.debug("path: " + curFile.getPath() +
                         "  |  pathcalled: " + path.getPath());
             }
+        }
+    }
+
+    public static boolean isValidDirectory(File curFile){
+        if (curFile.isDirectory() && curFile!=null && !curFile.isHidden()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean isValidFile(File curFile){
+        if (!curFile.isDirectory() && curFile!=null && !curFile.isHidden()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void hashFile(File curFile){
+        try{
+        String hashAlgo = new String(config.getConfig("HASH_ALGO"));
+        String file = "";
+        String hash = "";
+        try {
+            file = curFile.getPath();
+            log.debug("File: " + file);
+            hash = hasher.getHash(curFile.getPath(), hashAlgo, BUFFER);
+        } catch (IOException | HasherException e1) {
+            log.error("Failed to access and/or hash file!", e1);
+        }
+        sql.writeRecord(file, hash);
+        log.debug("\n\t Hash: " + hash);
+        }catch( Exception e){
+            log.warn(e.getStackTrace());
         }
     }
 }
